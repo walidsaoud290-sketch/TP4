ab3c8c58504ce656fba587c7c81047d68a71cd8f
walid <walidSaoud@gmail.com>
2026-03-12 19:58:45 +0000

    new modification

---
+++ app/user_manager.py
@@ -0,0 +1,22 @@
+class UserManager:
+    def __init__(self):
+        self.users = []
+
+    def add_user(self,username):
+        if not username:
+            raise ValueError("Le nom d'utilisateur est obligatoire")
+        if username in self.users:
+            raise ValueError("User deja existant")
+        self.users.append(username)
+
+    def remove_user(self,username):
+        if username not in self.users:
+            raise ValueError("User not found")
+        self.users.remove(username)
+
+    def count_users(self):
+        return len(self.users)
+
+def count_total_users(users):
+    temp=0
+    return len(users)
