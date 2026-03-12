700811f01b661fe4b4cb833f790184e91e194e00
walid <walidSaoud@gmail.com>
2026-03-12 20:07:15 +0000

    user_manager changing

--- app/user_manager.py
+++ app/user_manager.py
@@ -2,14 +2,14 @@ class UserManager:
     def __init__(self):
         self.users = []
 
-    def add_user(self,username):
+    def add_user(self, username):
         if not username:
             raise ValueError("Le nom d'utilisateur est obligatoire")
         if username in self.users:
             raise ValueError("User deja existant")
         self.users.append(username)
 
-    def remove_user(self,username):
+    def remove_user(self, username):
         if username not in self.users:
             raise ValueError("User not found")
         self.users.remove(username)
@@ -17,6 +17,6 @@ class UserManager:
     def count_users(self):
         return len(self.users)
 
+
 def count_total_users(users):
-    temp=0
     return len(users)
